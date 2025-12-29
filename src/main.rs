ff97beecbd49ac9df66b37b4cfc6df0a31761ca0
qtf0x <qtf0x@pm.me>
2025-12-29 16:29:33 -0700

    extracted argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {query}");
     println!("In file {file_path}");
@@ -12,4 +11,8 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    (&args[1], &args[2])
 }
