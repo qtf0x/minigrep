54bcf8c2752777166ece6a17a1efa0c75cee5578
qtf0x <qtf0x@pm.me>
2025-12-29 20:32:44 -0700

    implemented search_case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     res
 }
 
-pub fn search_case_insensitive<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    todo!();
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut res = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            res.push(line);
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
