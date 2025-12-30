da1f79cb9b2c1b95107a6d3b1dabae69cde14212
qtf0x <qtf0x@pm.me>
2025-12-29 20:20:52 -0700

    implemented search fn (naive)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    Vec::new()
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
