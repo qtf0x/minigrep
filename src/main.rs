057dd60acf7fc0e2601d4fc34badfa59fa33a39e
qtf0x <qtf0x@pm.me>
2025-12-29 20:43:44 -0700

    print errors to sterr, not stdout

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(err) = run(cfg) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     }
 }
